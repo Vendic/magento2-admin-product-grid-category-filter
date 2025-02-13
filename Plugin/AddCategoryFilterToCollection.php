b2f647e537d713967e88ce56073ad2d98057afd7
Tjitse Efdé <tjitse@vendic.nl>
2025-02-13 17:27:36 +0100

    Phpstan fix

--- Plugin/AddCategoryFilterToCollection.php
+++ Plugin/AddCategoryFilterToCollection.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Vendic\AdminProductGridCategoryFilter\Plugin;
 
+use Magento\Catalog\Ui\DataProvider\Product\ProductCollection;
 use Magento\Catalog\Ui\DataProvider\Product\ProductDataProvider;
 use Magento\Framework\Api\Filter;
 
@@ -12,7 +13,9 @@ class AddCategoryFilterToCollection
     public function aroundAddFilter(ProductDataProvider $subject, callable $proceed, Filter $filter)
     {
         if ($filter->getField() == 'category_id') {
-            return $subject->getCollection()->addCategoriesFilter(array('in' => $filter->getValue()));
+            /** @var ProductCollection $collection */
+            $collection = $subject->getCollection();
+            return $collection->addCategoriesFilter(['in' => $filter->getValue()]);
         }
 
         return $proceed($filter);
