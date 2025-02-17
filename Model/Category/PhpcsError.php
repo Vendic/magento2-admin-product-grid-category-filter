e2e86812f431d96d5f196d849f83b869d17bdad4
Tjitse Efdé <tjitse@vendic.nl>
2025-02-17 13:24:43 +0100

    Trigger phpcs error

---
+++ Model/Category/PhpcsError.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @copyright   Copyright (c) Vendic B.V https://vendic.nl/
+ */
+declare(strict_types=1);
+
+namespace Vendic\AdminProductGridCategoryFilter\Model\Category;
+
+class PhpcsError
+{
+    public function __construct(
+
+
+
+
+
+
+    )
+    {
+
+
+
+    }
+
+    public function testReturn() { return; }
+}
