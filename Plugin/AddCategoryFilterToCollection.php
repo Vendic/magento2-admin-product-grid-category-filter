5d75f6d07a1712745377a8f8971ea26ae46fc203
Tjitse <Tjitse@vendic.nl>
2025-02-17 14:54:50 +0100

    Add CI pipelines (#1)

--- Plugin/AddCategoryFilterToCollection.php
+++ Plugin/AddCategoryFilterToCollection.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace Vendic\AdminProductGridCategoryFilter\Plugin;
 
+use Magento\Catalog\Ui\DataProvider\Product\ProductCollection;
 use Magento\Catalog\Ui\DataProvider\Product\ProductDataProvider;
+use Magento\Framework\Api\Filter;
 
 class AddCategoryFilterToCollection
 {
-    public function aroundAddFilter(ProductDataProvider $subject, callable $proceed, \Magento\Framework\Api\Filter $filter)
+    public function aroundAddFilter(ProductDataProvider $subject, callable $proceed, Filter $filter)
     {
         if ($filter->getField() == 'category_id') {
-            return $subject->getCollection()->addCategoriesFilter(array('in' => $filter->getValue()));
+            /** @var ProductCollection $collection */
+            $collection = $subject->getCollection();
+            return $collection->addCategoriesFilter(['in' => $filter->getValue()]);
         }
 
         return $proceed($filter);
